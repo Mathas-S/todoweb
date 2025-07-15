1c62bc2d6e7fa1c228067a327eeeff890705a100
Mathas-S <mathas.sornp@gmail.com>
2025-07-15 11:37:05 +0700

    Logout Buttton Added

--- src/pages/profilepage/profile.jsx
+++ src/pages/profilepage/profile.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate } from 'react-router-dom'
 import './profile.css'
-const profile = () => {
+
+const Profile = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
   return (
     <>
-    <div className='profile-container'>
-      <h1>Profile Page</h1>
-    </div>
+      <div className='profile-container'>
+        <h1>Profile Page</h1>
+        <button onClick={handleLogout} className="logout-button">Logout</button>
+      </div>
     </>
   )
 }
 
-export default profile
+export default Profile
